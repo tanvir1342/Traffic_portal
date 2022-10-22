1a6915e1a0ac92b147c8945161587e2fc44dc579
Your Name <kasifatmolla0@gmail.com>
2022-10-23 01:51:38 +0600

    Create Area Table SIFAT 1

---
+++ trafficPortal/database/migrations/2022_10_22_194553_create_areas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->id();
+            $table->string('area_id');
+            $table->string('a_name');
+            $table->string('a_incharge_name');
+            $table->string('emplyee_id');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('areas');
+    }
+};
