583f8763a070ce521d09f92b428ffbc82c7ddf53
Your Name <kasifatmolla0@gmail.com>
2022-10-24 10:49:19 +0600

    Seeder setting

--- trafficPortal/database/seeders/DatabaseSeeder.php
+++ trafficPortal/database/seeders/DatabaseSeeder.php
@@ -20,5 +20,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            AreaSeeder::class,
+            TrafficPointSeeder::class,
+            TrafficPointStatusSeeder::class,
+            DutyListSeeder::class,
+        ]);
     }
 }
