48e5e83ebd9577c986a42bb5cedb47a2389fd28b
Your Name <kasifatmolla0@gmail.com>
2022-10-25 00:14:00 +0600

    Civil employeetable

---
+++ trafficPortal/database/migrations/2022_10_24_180747_create_civil_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('civil_employees', function (Blueprint $table) {
+            $table->bigInteger('ce_id');
+            $table->string('ce_name');
+            $table->string('organization_name');
+            $table->bigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('civil_employees');
+    }
+};
